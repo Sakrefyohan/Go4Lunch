8b6f2d8e33a83b4487db8e8c09fdb6c237155213
Sakrefyohan <yohan.sakref@gmail.com>
2020-09-10 00:13:06 +0200

    Added facebook login and Google TODO : Button Login For google

--- app/src/main/java/sakref/yohan/go4lunch/ui/MainActivity.java
+++ app/src/main/java/sakref/yohan/go4lunch/ui/MainActivity.java
@@ -1,4 +1,53 @@
 package sakref.yohan.go4lunch.ui;
 
-class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+import java.util.Arrays;
+
+import sakref.yohan.go4lunch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    /*
+    @OnClick(R.id.main_activity_button_login)
+    public void onClickLoginButton() {
+        // 3 - Launch Sign-In Activity when user clicked on Login Button
+        this.startSignInActivity();
+    }
+ */
+
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(), //EMAIL
+                                        new AuthUI.IdpConfig.GoogleBuilder().build(), //GOOGLE
+                                        new AuthUI.IdpConfig.FacebookBuilder().build())) // FACEBOOK
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_login)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
 }
