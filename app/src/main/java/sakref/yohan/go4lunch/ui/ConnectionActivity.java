c7b5ac3580b9ff207487121860e272fe31b0ebe1
Sakrefyohan <yohan.sakref@gmail.com>
2021-09-28 22:12:10 +0200

    Changing ConnectionActivity.java to get the user first before authentication.

--- app/src/main/java/sakref/yohan/go4lunch/ui/ConnectionActivity.java
+++ app/src/main/java/sakref/yohan/go4lunch/ui/ConnectionActivity.java
@@ -276,8 +276,10 @@ public class ConnectionActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
-                            user.sendEmailVerification();
+                            user = mAuth.getCurrentUser();
                             fetchWorkmate();
+                            user.sendEmailVerification();
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
